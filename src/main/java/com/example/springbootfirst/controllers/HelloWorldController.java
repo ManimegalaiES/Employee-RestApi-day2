05e0e05ad6f10f834f79a62b140e03ded0297eba
ManimegalaiES <manimegalai.es2023cse@sece.ac.in>
2025-07-06 13:05:25 +0530

    Crud Operations

--- src/main/java/com/example/springbootfirst/controllers/HelloWorldController.java
+++ src/main/java/com/example/springbootfirst/controllers/HelloWorldController.java
@@ -8,28 +8,39 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@RequestMapping("/employee")
 public class HelloWorldController {
 
     @Autowired
     private HelloWorldService hws;
 
-    @GetMapping("/")
+    @GetMapping
     public List<Employee> getMethod(){
+
         return hws.getMethod();
     }
-    @PostMapping("/")
+
+    @GetMapping("/{empid}")
+    public Employee getEmployeeById(@PathVariable int empid)
+    {
+        return hws.getEmployeeById(empid);
+    }
+
+    @PostMapping
     public String postMethod(@RequestBody Employee employee){
         hws.postmethodhws(employee);
         return "Employee added successfully";
     }
-    @PutMapping("/")
-    public String putMethod(){
-        return hws.putMethod();
+    @PutMapping("/{empid}")
+    public String putMethod(@RequestBody Employee employee){
+        return hws.putMethod(employee);
     }
-    @DeleteMapping("/")
+    @DeleteMapping
     public String DeleteMethod(){
         return hws.DeleteMethod();
     }
-
-
+    @DeleteMapping("/{empid}")
+    public void DeleteById(@PathVariable int empid){
+        hws.DeleteById(empid);
+    }
 }
--- src/main/java/com/example/springbootfirst/services/HelloWorldService.java
+++ src/main/java/com/example/springbootfirst/services/HelloWorldService.java
@@ -17,15 +17,71 @@ public class HelloWorldService {
         return emp;
     }
 
+    public Employee getEmployeeById(int empid) {
+        int ind=0;
+        boolean flag=false;
+        for(int i=0;i<emp.size();i++){
+            if(empid==emp.get(i).getEmpid()){
+                System.out.println("Emp_Id: "+emp.get(i).getEmpid()+emp.get(i));
+                ind=i;
+                flag=true;
+                break;
+            }
+        }
+        if(flag){
+           return emp.get(ind);
+        }else{
+            return new Employee();
+        }
+
+    }
+
     public void postmethodhws(Employee employee) {
         emp.add(employee);
     }
 
-    public String putMethod() {
-        return "This is Put Method";
+
+    public String DeleteMethod(){
+        return "This is Delete Method";
     }
 
-    public String DeleteMethod() {
-        return "This is Delete Method";
+
+    public void DeleteById(int empid) {
+        int ind=0;
+        boolean flag=false;
+        for(int i=0;i<emp.size();i++){
+            if(empid==emp.get(i).getEmpid()){
+                System.out.println("Emp_Id: "+emp.get(i).getEmpid()+emp.get(i));
+                ind=i;
+                flag=true;
+                break;
+            }
+        }
+        if(flag){
+            emp.remove(ind);
+        }else{
+            System.out.println("Employee no found");
+        }
+    }
+
+    public String putMethod(Employee employee) {
+        int ind=0;
+        boolean flag=false;
+        for(int i=0;i<emp.size();i++){
+            if(employee.getEmpid()==emp.get(i).getEmpid()){
+                System.out.println("Emp_Id: "+emp.get(i).getEmpid()+emp.get(i));
+                ind=i;
+                flag=true;
+                break;
+            }
+        }
+        if(flag){
+            emp.set(ind,employee);
+            return "Employee updated successfully";
+        }else{
+            System.out.println("Employee no found");
+            return "Employee not updated";
+        }
+
     }
 }
