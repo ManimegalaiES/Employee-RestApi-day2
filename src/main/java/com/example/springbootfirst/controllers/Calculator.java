459c5c5875bba0ab350d5b251ee51892ddddf833
ManimegalaiES <manimegalai.es2023cse@sece.ac.in>
2025-07-14 21:24:37 +0530

    Day 10 Task Calculator Test

---
+++ src/main/java/com/example/springbootfirst/controllers/Calculator.java
@@ -0,0 +1,19 @@
+package com.example.springbootfirst.controllers;
+
+public class Calculator {
+    public int add(int a,int b) {
+        return a+b;
+    }
+    public int subtract(int a,int b) {
+        return a-b;
+    }
+    public int multiply(int a,int b) {
+        return a*b;
+    }
+    public int divide(int a,int b) {
+        if (b==0){
+            System.out.println("Cannot divide by zero");
+        }
+        return a/b;
+    }
+}
